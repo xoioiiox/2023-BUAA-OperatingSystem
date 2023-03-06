c9dbca4f6d0695030c27e4e2a0586a35c237e514
彭佳琦 <21373236@buaa.edu.cn>
2023-03-06 20:18:40 +0800

    first

--- analyze.sh
+++ analyze.sh
@@ -2,19 +2,23 @@
 
 if [ $# -eq 1 ];
 then
+	grep -E "WARN|ERROR" $1 >> bug.txt
     # Your code here. (1/4)
 
 else
     case $2 in
     "--latest")
+    	tail --lines=5 $1
         # Your code here. (2/4)
 
     ;;
     "--find")
+    	grep $3 $1 >> $3.txt
         # Your code here. (3/4)
 
     ;;
     "--diff")
+    	diff -q $1 $3 
         # Your code here. (4/4)
 
     ;;
