441d6287f168fbedfda63f13fee45a3df3cb854d
彭佳琦 <21373236@buaa.edu.cn>
2023-03-14 19:49:32 +0800

    3

--- kern/tlbex.c
+++ kern/tlbex.c
@@ -42,6 +42,14 @@ Pte _do_tlb_refill(u_long va, u_int asid) {
 	 */
 
 	/* Exercise 2.9: Your code here. */
+	while (1) {
+		if (page_lookup(cur_pgdir, va, &pte) == NULL) {
+			passive_alloc(va, cur_pgdir, asid);
+		}
+		else {
+			break;
+		}
+	}
 
 	return *pte;
 }
