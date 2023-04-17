e74092e8e3e2f716ee9880ebedb6f6e929e6a119
彭佳琦 <21373236@buaa.edu.cn>
2023-04-17 19:30:05 +0800

    first

--- kern/sched.c
+++ kern/sched.c
@@ -17,6 +17,7 @@
 void schedule(int yield) {
 	static int count = 0; // remaining time slices of current env
 	struct Env *e = curenv;
+	static int user_time[5];
 
 	/* We always decrease the 'count' by 1.
 	 *
@@ -35,14 +36,37 @@ void schedule(int yield) {
 	 *   'TAILQ_FIRST', 'TAILQ_REMOVE', 'TAILQ_INSERT_TAIL'
 	 */
 	/* Exercise 3.12: Your code here. */
+	int user[5] = {0};
+	int i;
+	int min = 10000;
+	int min_user;
+	struct Env *var;
+
+	TAILQ_FOREACH(var, &env_sched_list, env_sched_link) {
+		user[var->env_user] = 1;
+	}
 	if (yield || e == NULL || count == 0 || e->env_status != ENV_RUNNABLE) {
 		if ((e != NULL) && (e->env_status == ENV_RUNNABLE)) {
 			TAILQ_REMOVE(&env_sched_list, e, env_sched_link);
 			TAILQ_INSERT_TAIL(&env_sched_list, e, env_sched_link);
+			user_time[e->env_user] += e->env_pri;
 		}
-		e = TAILQ_FIRST(&env_sched_list);
-		if (e == NULL) {
+		if (TAILQ_EMPTY(&env_sched_list)) {
 			panic("schedule: no runnable envs");
+		}
+		for(i = 0; i < 5; i++) {
+			if (user[i] != 0) {
+				if (min > user_time[i]) {
+					min = user_time[i];
+					min_user = i;
+				}
+			}
+		}
+		TAILQ_FOREACH(var, &env_sched_list, env_sched_link) {
+			if (var->env_user == min_user) {
+				e = var;
+				break;
+			}
 		}
 		count = e->env_pri;
 	}
