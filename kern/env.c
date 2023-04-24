ea42db030c0acf9db6331c18dbb2fbc6f6ce27d8
彭佳琦 <21373236@buaa.edu.cn>
2023-04-24 19:30:38 +0800

    add

--- kern/env.c
+++ kern/env.c
@@ -275,7 +275,7 @@ int env_alloc(struct Env **new, u_int parent_id) {
 	e->env_id = mkenvid(e);
 	e->env_parent_id = parent_id;
 	asid_alloc(&(e->env_asid));
-
+	e->env_gid = 0;
 
 	/* Step 4: Initialize the sp and 'cp0_status' in 'e->env_tf'. */
 	// Timer interrupt (STATUS_IM4) will be enabled.
