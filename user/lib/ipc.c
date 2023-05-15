5dfdcdfa5cd9e4b152dd67ff9a676f243a04ca5c
彭佳琦 <21373236@buaa.edu.cn>
2023-05-15 19:18:32 +0800

    v1

--- user/lib/ipc.c
+++ user/lib/ipc.c
@@ -4,6 +4,37 @@
 #include <lib.h>
 #include <mmu.h>
 
+u_int get_time(u_int *us) {
+	u_int tmp = 0;
+	u_int sum = 0;
+	u_int n = 0;
+	syscall_read_dev(&tmp, 0x15000000, 4);
+	syscall_read_dev(&sum, 0x15000010, 4);
+	syscall_read_dev(&n, 0x15000020, 4);
+	*us = n;
+	return sum;
+}
+
+void usleep(u_int us) {
+	// 读取进程进入 usleep 函数的时间
+	u_int start;
+	u_int start_u;
+	u_int tmp;
+	u_int cur;
+	u_int cur_u;
+	start = get_time(&start_u);
+	while (1) {
+		// 读取当前时间
+		cur = get_time(&cur_u);
+		if (((int)cur - (int)start) * 100000 >= ((int)start_u + (int)us - (int)cur_u)/* 当前时间 >= 进入时间 + us 微秒*/) {
+			return;
+		} else {
+			syscall_yield();
+			// 进程切换
+		}
+	}
+}
+
 // Send val to whom.  This function keeps trying until
 // it succeeds.  It should panic() on any error other than
 // -E_IPC_NOT_RECV.
